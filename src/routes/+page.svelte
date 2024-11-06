7c80e6a0a9f091555e3b2a2755febd106c1d150a
jis3r <jis3r@protonmail.com>
2024-11-06 21:30:05 +0100

    add: icon-listing with cards and props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,39 @@
 <script>
 	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card/index.js';
+
 	import { Sun } from 'lucide-svelte';
 	import { Moon } from 'lucide-svelte';
 	import ArrowLeft from '$lib/icons/arrowLeft.svelte';
 	import ArrowRight from '$lib/icons/arrowRight.svelte';
 	import Clock from '$lib/icons/clock.svelte';
 	import Expand from '$lib/icons/expand.svelte';
+	import Unplug from '$lib/icons/unplug.svelte';
 
 	import { toggleMode } from 'mode-watcher';
+
+	let icons = [
+		{
+			name: 'arrow-left',
+			component: ArrowLeft
+		},
+		{
+			name: 'arrow-right',
+			component: ArrowRight
+		},
+		{
+			name: 'clock',
+			component: Clock
+		},
+		{
+			name: 'expand',
+			component: Expand
+		},
+		{
+			name: 'unplug',
+			component: Unplug
+		}
+	];
 </script>
 
 <header class="container flex h-full w-full max-w-7xl items-center justify-between py-4">
@@ -32,18 +58,18 @@
 	<div class="container max-w-7xl">
 		<h1 class="text-balance text-2xl sm:text-3xl">
 			beautifully crafted, moving icons. for <span
-				class="from-svelte bg-gradient-to-br to-orange-400 bg-clip-text text-transparent"
+				class="bg-gradient-to-br from-svelte to-orange-400 bg-clip-text text-transparent"
 				>svelte</span
 			>.
 		</h1>
 		<p
-			class="text-muted-foreground mt-2 max-w-lg text-pretty text-xs leading-relaxed sm:mt-4 sm:text-sm"
+			class="mt-2 max-w-lg text-pretty text-xs leading-relaxed text-muted-foreground sm:mt-4 sm:text-sm"
 		>
 			an open-source collection of smooth, animated icons for your projects. feel free to use them,
 			share your feedback, and let's make this library awesome together!
 		</p>
 
-		<p class="text-muted-foreground mt-4 text-xs sm:text-sm">
+		<p class="mt-4 text-xs text-muted-foreground sm:text-sm">
 			built with
 			<Button
 				variant="secondary"
@@ -57,11 +83,21 @@
 				href="https://github.com/pqoqubbw">@pqoqubbw</Button
 			>
 		</p>
-		<div class="flex gap-1 p-7">
-			<ArrowLeft />
-			<ArrowRight />
-			<Clock />
-			<Expand />
+		<div class="my-10 flex flex-col gap-6 sm:my-20">
+			<div
+				class="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-[repeat(auto-fill,minmax(165px,1fr))]"
+			>
+				{#each icons as icon}
+					<div
+						class="flex h-full w-full flex-col items-center justify-center rounded-md border border-input p-3"
+					>
+						<icon.component
+							classes="flex cursor-pointer select-none items-center justify-center rounded-md p-2 transition-colors duration-200 hover:bg-accent"
+						/>
+						<p class="mb-3 mt-5 text-center text-xs text-muted-foreground">{icon.name}</p>
+					</div>
+				{/each}
+			</div>
 		</div>
 	</div>
 </main>
