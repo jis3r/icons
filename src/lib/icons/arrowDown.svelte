e2f033e2c46358ce80e199aed9fe98c07abe0c5d
jis3r <jis3r@protonmail.com>
2024-11-06 22:17:25 +0100

    add: arrow-down icon

---
+++ src/lib/icons/arrowDown.svelte
@@ -0,0 +1,39 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+
+		setTimeout(() => {
+			isHovered = false;
+		}, 200);
+	}
+</script>
+
+<div class={classes} aria-label="arrow-down" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="m5 12 7 7 7-7" class:head={isHovered} />
+		<path d="M12 14v5" class:head={isHovered} />
+		<path d="M12 5v9" />
+	</svg>
+</div>
+
+<style>
+	path {
+		transition: all 0.2s ease-in;
+	}
+
+	.head {
+		transform: translateY(-3px);
+	}
+</style>
