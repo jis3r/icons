6c16dad7e75a3b40cac7b34649e0169ce5330e5c
jis3r <jis3r@protonmail.com>
2024-11-08 14:50:29 +0100

    add: vite-plugin-raw

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import rawPlugin from 'vite-plugin-raw';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		rawPlugin({
+			match: /\/components\/icons\/.*\.svelte$/
+		})
+	],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
