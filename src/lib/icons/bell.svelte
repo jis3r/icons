14695f7aafcf3a79987ae86a90f5bed09aec2b98
jis3r <jis3r@protonmail.com>
2024-11-09 18:01:05 +0100

    add: bell icon

---
+++ src/lib/icons/bell.svelte
@@ -0,0 +1,59 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="bell-icon"
+	role="img"
+	onmouseenter={handleMouseEnter}
+	onmouseleave={handleMouseLeave}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class:animate-svg={isHovered}
+	>
+		<path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+		<path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+	</svg>
+</div>
+
+<style>
+	.animate-svg {
+		animation: rotateAnimation 0.5s ease-in-out;
+	}
+
+	@keyframes rotateAnimation {
+		0% {
+			transform: rotate(0deg);
+		}
+		20% {
+			transform: rotate(-10deg);
+		}
+		40% {
+			transform: rotate(10deg);
+		}
+		60% {
+			transform: rotate(-10deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+</style>
