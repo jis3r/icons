0c78fab868416fe7b25a49bf346c9d1a29f03499
jis3r <jis3r@protonmail.com>
2024-11-09 17:54:07 +0100

    add: circle-help icon

---
+++ src/lib/icons/circle-help.svelte
@@ -0,0 +1,71 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="circle-help-icon"
+	role="img"
+	onmouseenter={handleMouseEnter}
+	onmouseleave={handleMouseLeave}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class:animate-icon={isHovered}
+	>
+		<circle cx="12" cy="12" r="10" />
+		<g class:animate-path={isHovered}>
+			<path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+			<path d="M12 17h.01" />
+		</g>
+	</svg>
+</div>
+
+<style>
+	.animate-icon {
+		transition: transform 0.5s ease-in-out;
+	}
+
+	.animate-path {
+		transition: transform 0.5s ease-in-out;
+		transform-origin: center;
+	}
+
+	div:hover .animate-path {
+		animation: rotateAnimation 0.5s ease-in-out;
+	}
+
+	@keyframes rotateAnimation {
+		0% {
+			transform: rotate(0deg);
+		}
+		20% {
+			transform: rotate(-10deg);
+		}
+		40% {
+			transform: rotate(10deg);
+		}
+		60% {
+			transform: rotate(-10deg);
+		}
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+</style>
