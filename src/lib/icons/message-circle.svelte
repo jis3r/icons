418375513ea40e6fc5bba0d39bdca4648baa37f8
jis3r <jis3r@protonmail.com>
2024-11-09 17:50:43 +0100

    add: message-circle

---
+++ src/lib/icons/message-circle.svelte
@@ -0,0 +1,55 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="message-circle-icon"
+	role="img"
+	onmouseenter={handleMouseEnter}
+	onmouseleave={handleMouseLeave}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class:animate-icon={isHovered}
+	>
+		<path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+	</svg>
+</div>
+
+<style>
+	.animate-icon {
+		animation: primaryAnimation 0.5s ease-in-out;
+	}
+
+	@keyframes primaryAnimation {
+		0% {
+			transform: scale(1) rotate(0deg);
+		}
+		20% {
+			transform: scale(1.05) rotate(-7deg);
+		}
+		40% {
+			transform: scale(1.05) rotate(7deg);
+		}
+		100% {
+			transform: scale(1) rotate(0deg);
+		}
+	}
+</style>
