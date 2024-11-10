ab82ea5462f5b95c391ebe90fb96776cbdfed191
jis3r <jis3r@protonmail.com>
2024-11-10 20:36:04 +0100

    add: square-chevron-down icon

---
+++ src/lib/icons/square-chevron-down.svelte
@@ -0,0 +1,54 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+
+		setTimeout(() => {
+			isHovered = false;
+		}, 300);
+	}
+</script>
+
+<div class={classes} aria-label="square-chevron-down" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="square-chevron-down"
+		class:animate={isHovered}
+	>
+		<rect width="18" height="18" x="3" y="3" rx="2" />
+		<path d="m16 10-4 4-4-4" class="chevron" />
+	</svg>
+</div>
+
+<style>
+	.square-chevron-down {
+		overflow: visible;
+	}
+
+	.chevron {
+		transition: transform 0.3s ease-in;
+	}
+
+	.square-chevron-down.animate .chevron {
+		animation: bounceChevron 0.3s ease-in;
+	}
+
+	@keyframes bounceChevron {
+		0%,
+		100% {
+			transform: translateY(0);
+		}
+		40% {
+			transform: translateY(2px);
+		}
+	}
+</style>
