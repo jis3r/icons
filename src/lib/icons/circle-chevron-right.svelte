f87fd26ee50aed27e22155b9f5ca8efa43c74d98
jis3r <jis3r@protonmail.com>
2024-11-10 20:02:31 +0100

    add: circle-chevron-right icon

---
+++ src/lib/icons/circle-chevron-right.svelte
@@ -0,0 +1,54 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+
+		setTimeout(() => {
+			isHovered = false;
+		}, 500);
+	}
+</script>
+
+<div class={classes} aria-label="circle-chevron-right" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="circle-chevron-right"
+		class:animate={isHovered}
+	>
+		<circle cx="12" cy="12" r="10" />
+		<path d="m10 8 4 4-4 4" class="chevron" />
+	</svg>
+</div>
+
+<style>
+	.circle-chevron-right {
+		overflow: visible;
+	}
+
+	.chevron {
+		transition: transform 0.3s ease-in;
+	}
+
+	.circle-chevron-right.animate .chevron {
+		animation: bounceChevron 0.3s ease-in;
+	}
+
+	@keyframes bounceChevron {
+		0%,
+		100% {
+			transform: translateX(0);
+		}
+		40% {
+			transform: translateX(2px);
+		}
+	}
+</style>
