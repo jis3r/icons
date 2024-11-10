d920c3cd36dde30f5f7041e4e7e7c0621602be8a
jis3r <jis3r@protonmail.com>
2024-11-10 20:20:16 +0100

    add: chevron-down icon

---
+++ src/lib/icons/chevron-down.svelte
@@ -0,0 +1,37 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+
+		setTimeout(() => {
+			isHovered = false;
+		}, 200);
+	}
+</script>
+
+<div class={classes} aria-label="chevron-down" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="m6 9 6 6 6-6" class:chevron={isHovered} />
+	</svg>
+</div>
+
+<style>
+	path {
+		transition: all 0.2s ease-in;
+	}
+
+	.chevron {
+		transform: translateY(3px);
+	}
+</style>
