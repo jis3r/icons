365eeaf4c398bd2efa0d95d674b29e872769a5b6
jis3r <jis3r@protonmail.com>
2024-11-12 00:04:35 +0100

    add: undo icon

---
+++ src/lib/icons/undo.svelte
@@ -0,0 +1,47 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="undo"
+	role="img"
+	onmouseenter={handleMouseEnter}
+	onmouseleave={handleMouseLeave}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="undo-icon"
+		class:animate={isHovered}
+	>
+		<path d="M3 7v6h6" />
+		<path d="M21 17a9 9 0 0 0-9-9 9 9 0 0 0-6 2.3L3 13" />
+	</svg>
+</div>
+
+<style>
+	.undo-icon {
+		transform-origin: 14px 20px;
+		transition: transform 0.3s ease;
+	}
+
+	.undo-icon.animate {
+		transform: rotate(-10deg);
+	}
+</style>
