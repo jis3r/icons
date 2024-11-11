be51758e5d6bf9033727aec98b5c556508e174a1
jis3r <jis3r@protonmail.com>
2024-11-12 00:11:53 +0100

    add: redo icon

---
+++ src/lib/icons/redo.svelte
@@ -0,0 +1,47 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="redo"
+	role="img"
+	onmouseenter={handleMouseEnter}
+	onmouseleave={handleMouseLeave}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="redo-icon"
+		class:animate={isHovered}
+	>
+		<path d="M21 7v6h-6" />
+		<path d="M3 17a9 9 0 0 1 9-9 9 9 0 0 1 6 2.3l3 2.7" />
+	</svg>
+</div>
+
+<style>
+	.redo-icon {
+		transform-origin: 14px 20px;
+		transition: transform 0.3s ease;
+	}
+
+	.redo-icon.animate {
+		transform: rotate(13deg);
+	}
+</style>
