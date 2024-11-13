4e7d7f87e5f42bbe9aa2aea5e1fbbe7cc272bbdb
jis3r <jis3r@protonmail.com>
2024-11-13 01:33:14 +0100

    fix: pencil aria-label

--- src/lib/icons/pencil.svelte
+++ src/lib/icons/pencil.svelte
@@ -12,7 +12,7 @@
 
 <div
 	class={classes}
-	aria-label="pencil-icon"
+	aria-label="pencil"
 	role="img"
 	onmouseenter={handleMouseEnter}
 	onmouseleave={handleMouseLeave}
@@ -30,7 +30,10 @@
 		class="pencil-icon"
 		class:animate={isHovered}
 	>
-		<path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" class="" />
+		<path
+			d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"
+			class=""
+		/>
 		<path d="m15 5 4 4" class="" />
 	</svg>
 </div>
