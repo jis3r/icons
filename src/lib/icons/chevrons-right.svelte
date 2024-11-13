11a6af91db8cdd4c696a56abeb3ec99e2f9d81d9
jis3r <jis3r@protonmail.com>
2024-11-13 01:11:27 +0100

    add: chevrons-right icon

---
+++ src/lib/icons/chevrons-right.svelte
@@ -0,0 +1,41 @@
+<script>
+	let { color = 'currentColor', isHovered = false, classes = '' } = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+
+		setTimeout(() => {
+			isHovered = false;
+		}, 200);
+	}
+</script>
+
+<div class={classes} aria-label="chevrons-right" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="28"
+		height="28"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width="2"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="chevrons-right-icon"
+		class:chevron-right={isHovered}
+	>
+		<path d="m6 17 5-5-5-5" />
+		<path d="m13 17 5-5-5-5" />
+	</svg>
+</div>
+
+<style>
+	.chevrons-right-icon {
+		overflow: visible;
+		transition: all 0.2s ease-in;
+	}
+
+	.chevron-right {
+		transform: translateX(3px);
+	}
+</style>
