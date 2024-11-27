7457f329a196a2c058c93e3377e778fb555b9717
jis3r <jis3r@protonmail.com>
2024-11-27 22:28:49 +0100

    fix: activity missing properties

--- src/lib/icons/activity.svelte
+++ src/lib/icons/activity.svelte
@@ -24,12 +24,12 @@
 >
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		width="28"
-		height="28"
+		width={size}
+		height={size}
 		viewBox="0 0 24 24"
 		fill="none"
 		stroke={color}
-		stroke-width="2"
+		stroke-width={strokeWidth}
 		stroke-linecap="round"
 		stroke-linejoin="round"
 		class="activity-icon"
