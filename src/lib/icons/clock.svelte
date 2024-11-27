69734c59d3babad5ba6f78d39592a2fd21f7c1c3
jis3r <jis3r@protonmail.com>
2024-11-27 22:27:59 +0100

    fix: corrected clock icon

--- src/lib/icons/clock.svelte
+++ src/lib/icons/clock.svelte
@@ -35,28 +35,28 @@
 		stroke-linejoin="round"
 	>
 		<circle cx="12" cy="12" r="10" />
-		<line x1="12" y1="12" x2="12" y2="6" class="hour-hand" class:animate={isHovered} />
-		<line x1="12" y1="12" x2="16" y2="12" class="minute-hand" class:animate={isHovered} />
+		<line x1="12" y1="6" x2="12" y2="12" class="minute-hand" class:animate={isHovered} />
+		<line x1="12" y1="12" x2="16" y2="14" class="hour-hand" class:animate={isHovered} />
 	</svg>
 </div>
 
 <style>
-	.hour-hand,
-	.minute-hand {
+	.minute-hand,
+	.hour-hand {
 		transform-origin: center;
 		transition: transform 0.6s cubic-bezier(0.4, 0, 0.2, 1);
 	}
 
-	.minute-hand {
+	.hour-hand {
 		transition-duration: 0.5s;
 		transition-timing-function: ease-in-out;
 	}
 
-	.hour-hand.animate {
+	.minute-hand.animate {
 		transform: rotate(360deg);
 	}
 
-	.minute-hand.animate {
-		transform: rotate(45deg);
+	.hour-hand.animate {
+		transform: rotate(30deg);
 	}
 </style>
