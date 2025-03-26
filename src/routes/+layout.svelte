b9fe10eeecbd8af71683cb75ac6c248be8f863a9
jis3r <jis3r@protonmail.com>
2025-03-27 00:57:42 +0100

    add: integrated new navbar component in global layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import Navbar from '$lib/components/navbar.svelte';
+
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
+
+<Navbar />
 
 {@render children()}
