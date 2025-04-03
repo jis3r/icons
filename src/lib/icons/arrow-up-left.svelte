dc74466fa0d78e4475c636381bc3a49721c4e839
jis3r <jis3r@protonmail.com>
2025-04-04 00:51:59 +0200

    add: diagonal arrow icons

---
+++ src/lib/icons/arrow-up-left.svelte
@@ -0,0 +1,44 @@
+<script>
+	let {
+		color = 'currentColor',
+		size = 28,
+		strokeWidth = 2,
+		isHovered = false,
+		classes = ''
+	} = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+		setTimeout(() => {
+			isHovered = false;
+		}, 200);
+	}
+</script>
+
+<div class={classes} aria-label="arrow-up-left" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={size}
+		height={size}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width={strokeWidth}
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="M7 17V7h10" class:head={isHovered} />
+		<path d="M12 12 L7 7" class:head={isHovered} />
+		<path d="M17 17 L12 12" />
+	</svg>
+</div>
+
+<style>
+	path {
+		transition: all 0.2s ease-out;
+	}
+	/* Arrow is pointing up-left, so head moves down-right */
+	.head {
+		transform: translate(3px, 3px);
+	}
+</style>
---
+++ src/lib/icons/arrow-up-right.svelte
@@ -0,0 +1,43 @@
+<script>
+	let {
+		color = 'currentColor',
+		size = 28,
+		strokeWidth = 2,
+		isHovered = false,
+		classes = ''
+	} = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+		setTimeout(() => {
+			isHovered = false;
+		}, 200);
+	}
+</script>
+
+<div class={classes} aria-label="arrow-up-right" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={size}
+		height={size}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width={strokeWidth}
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="M7 7h10v10" class:head={isHovered} />
+		<path d="M17 7 L12 12" class:head={isHovered} />
+		<path d="M7 17 L12 12" />
+	</svg>
+</div>
+
+<style>
+	path {
+		transition: all 0.2s ease-out;
+	}
+	.head {
+		transform: translate(-3px, 3px);
+	}
+</style>
