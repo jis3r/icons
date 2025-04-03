2fe209a2c2ed0b91a933011fbda50ed4fd1ad3e1
jis3r <jis3r@protonmail.com>
2025-04-04 00:52:43 +0200

    add: diagonal square arrow icons

---
+++ src/lib/icons/square-arrow-down-right.svelte
@@ -0,0 +1,49 @@
+<script>
+	let {
+		color = 'currentColor',
+		size = 28,
+		strokeWidth = 2,
+		isHovered = false,
+		classes = ''
+	} = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+		setTimeout(() => {
+			isHovered = false;
+		}, 200);
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="square-arrow-down-right"
+	role="img"
+	onmouseenter={handleMouseEnter}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={size}
+		height={size}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width={strokeWidth}
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<rect width="18" height="18" x="3" y="3" rx="2" />
+		<path d="M16 8v8H8" class:head={isHovered} />
+		<path d="M16 16 L12 12" class:head={isHovered} />
+		<path d="M8 8 L12 12" />
+	</svg>
+</div>
+
+<style>
+	path {
+		transition: all 0.2s ease-out;
+	}
+	.head {
+		transform: translate(-1.5px, -1.5px);
+	}
+</style>
