651e706b1d74359a5a509b7d02f79445bb46396e
jis3r <jis3r@protonmail.com>
2025-04-04 21:36:41 +0200

    add: arrow-big icons

---
+++ src/lib/icons/arrow-big-down.svelte
@@ -0,0 +1,40 @@
+<script>
+	let {
+		color = 'currentColor',
+		size = 28,
+		strokeWidth = 2,
+		isHovered = false,
+		classes = ''
+	} = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+		setTimeout(() => (isHovered = false), 200);
+	}
+</script>
+
+<div class={classes} aria-label="arrow-big-down" role="img" onmouseenter={handleMouseEnter}>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class:animate={isHovered}
+		width={size}
+		height={size}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width={strokeWidth}
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path d="M15 6v6h4l-7 7-7-7h4V6h6z" />
+	</svg>
+</div>
+
+<style>
+	svg {
+		transition: transform 0.2s ease-out;
+	}
+	.animate {
+		transform: translateY(3px);
+	}
+</style>
