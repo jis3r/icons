93627d458bbf0c8151b401e60c472e3efe0bba62
jis3r <jis3r@protonmail.com>
2025-04-04 22:38:43 +0200

    fix: update transition duration for refresh-ccw icon

--- src/lib/icons/refresh-ccw.svelte
+++ src/lib/icons/refresh-ccw.svelte
@@ -45,7 +45,7 @@
 
 <style>
 	.refresh-icon {
-		transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+		transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
 		transform-origin: center;
 	}
 
