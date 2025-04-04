6d93d247f666b44a825f59d20e61e2b125822441
jis3r <jis3r@protonmail.com>
2025-04-04 22:37:58 +0200

    add: rotate-cw icon

---
+++ src/lib/icons/rotate-cw.svelte
@@ -0,0 +1,56 @@
+<script>
+	let {
+		color = 'currentColor',
+		size = 28,
+		strokeWidth = 2,
+		isHovered = false,
+		classes = ''
+	} = $props();
+
+	function handleMouseEnter() {
+		isHovered = true;
+	}
+
+	function handleMouseLeave() {
+		isHovered = false;
+	}
+</script>
+
+<div
+	class={classes}
+	aria-label="rotate-cw"
+	role="img"
+	onmouseenter={handleMouseEnter}
+	onmouseleave={handleMouseLeave}
+>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={size}
+		height={size}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke={color}
+		stroke-width={strokeWidth}
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		class="rotate-cw-icon"
+		class:animate={isHovered}
+	>
+		<path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
+		<path d="M21 3v5h-5" />
+	</svg>
+</div>
+
+<style>
+	.rotate-cw-icon {
+		transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+		transform-origin: center;
+	}
+	.rotate-cw-icon.animate {
+		transform: rotate(50deg);
+	}
+
+	svg {
+		overflow: visible;
+	}
+</style>
