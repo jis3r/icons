663c12e37abe01a6b4edc8fff1dfa2cc3f4605e2
jis3r <jis3r@protonmail.com>
2025-05-17 17:17:26 +0200

    fix: adjusted new icons

--- src/lib/icons/briefcase-medical.svelte
+++ src/lib/icons/briefcase-medical.svelte
@@ -1,10 +1,20 @@
 <script>
+	/**
+	 * @typedef {Object} Props
+	 * @property {string} [color]
+	 * @property {number} [size]
+	 * @property {number} [strokeWidth]
+	 * @property {boolean} [isHovered]
+	 * @property {string} [class]
+	 */
+
+	/** @type {Props} */
 	let {
 		color = 'currentColor',
 		size = 24,
 		strokeWidth = 2,
 		isHovered = false,
-		classes = ''
+		class: className = ''
 	} = $props();
 
 	function handleMouseEnter() {
@@ -16,7 +26,7 @@
 	}
 </script>
 
-<div class={classes} aria-label="briefcase-medical" role="img" onmouseenter={handleMouseEnter}>
+<div class={className} aria-label="briefcase-medical" role="img" onmouseenter={handleMouseEnter}>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width={size}
