38f61a0a8672c6e27e376f60520a492c51375589
jis3r <jis3r@protonmail.com>
2025-05-17 18:07:33 +0200

    add: vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
 	import { ModeWatcher } from 'mode-watcher';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
 	let { children } = $props();
+
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <ModeWatcher />
