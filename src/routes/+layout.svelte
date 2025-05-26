aeddb029565b77884c9957f4ded3d2996e3591bf
jis3r <jis3r@protonmail.com>
2025-05-27 01:02:31 +0200

    add: PostHog for analytics tracking
    - Added PostHog as a dependency in package.json and package-lock.json.
    - Initialized PostHog in +layout.svelte to track user interactions on the client side.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,20 @@
 <script>
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
+	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if (browser) {
+			posthog.init('phc_MRu5fLKb2djv49nDpyoTQqni5TuQLC8k2pJOVyERMmE', {
+				api_host: 'https://eu.i.posthog.com',
+				person_profiles: 'always'
+			});
+		}
+	});
 </script>
 
 <ModeWatcher />
