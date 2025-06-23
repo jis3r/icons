e7ef954734a606bf232c245eb3c32ebe092cee0b
jis3r <jis3r@protonmail.com>
2025-06-23 22:55:00 +0200

    refactor: enhance bell icon animation and timing

--- src/lib/icons/bell.svelte
+++ src/lib/icons/bell.svelte
@@ -22,7 +22,7 @@
 
 		setTimeout(() => {
 			isHovered = false;
-		}, 500);
+		}, 1100);
 	}
 </script>
 
@@ -39,32 +39,76 @@
 		stroke-linejoin="round"
 		class:animate-svg={isHovered}
 	>
-		<path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-		<path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+		<path
+			d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326"
+			class:animate-bell={isHovered}
+		/>
+		<path d="M10.268 21a2 2 0 0 0 3.464 0" class:animate-clapper={isHovered} />
 	</svg>
 </div>
 
 <style>
 	.animate-svg {
-		transform-origin: top;
-		animation: rotateAnimation 0.5s ease-in-out;
+		transform-origin: top center;
+		animation: bellRing 0.9s ease-in-out;
 	}
 
-	@keyframes rotateAnimation {
+	.animate-bell {
+		animation: bellMove 1.1s ease-in-out;
+	}
+
+	.animate-clapper {
+		animation: clapperMove 1.1s ease-in-out;
+	}
+
+	@keyframes bellRing {
 		0% {
 			transform: rotate(0deg);
 		}
-		20% {
+		16.67% {
+			transform: rotate(20deg);
+		}
+		33.33% {
 			transform: rotate(-10deg);
 		}
-		40% {
+		50% {
 			transform: rotate(10deg);
 		}
-		60% {
-			transform: rotate(-10deg);
+		66.67% {
+			transform: rotate(-5deg);
+		}
+		83.33% {
+			transform: rotate(3deg);
 		}
 		100% {
 			transform: rotate(0deg);
+		}
+	}
+
+	@keyframes clapperMove {
+		0% {
+			transform: translateX(0);
+		}
+		14.29% {
+			transform: translateX(-6px);
+		}
+		28.57% {
+			transform: translateX(5px);
+		}
+		42.86% {
+			transform: translateX(-5px);
+		}
+		57.14% {
+			transform: translateX(4px);
+		}
+		71.43% {
+			transform: translateX(-3px);
+		}
+		85.71% {
+			transform: translateX(2px);
+		}
+		100% {
+			transform: translateX(0);
 		}
 	}
 </style>
