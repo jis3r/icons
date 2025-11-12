770f5607a73c63c80305dd24dc9d3deec1f27864
jis3r <jis3r@protonmail.com>
2025-11-13 00:19:23 +0100

    feat: enhance bookmark-check icon animation

--- src/lib/icons/bookmark-check.svelte
+++ src/lib/icons/bookmark-check.svelte
@@ -21,7 +21,7 @@
 		isHovered = true;
 		setTimeout(() => {
 			isHovered = false;
-		}, 500);
+		}, 600);
 	}
 </script>
 
@@ -39,7 +39,7 @@
 		class="bookmark-check-icon"
 		class:animate={isHovered}
 	>
-		<path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2Z" />
+		<path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2Z" class="bookmark-path" />
 		<path d="m9 10 2 2 4-4" class="check-path" />
 	</svg>
 </div>
@@ -51,6 +51,13 @@
 	.bookmark-check-icon {
 		overflow: visible;
 	}
+	.bookmark-path {
+		transform-origin: center;
+		transition: transform 0.3s ease-out;
+	}
+	.bookmark-check-icon.animate .bookmark-path {
+		animation: bookmarkBounce 0.6s ease-out;
+	}
 	.check-path {
 		stroke-dasharray: 9;
 		stroke-dashoffset: 0;
@@ -60,6 +67,23 @@
 	}
 	.bookmark-check-icon.animate .check-path {
 		animation: checkAnimation 0.5s ease-out backwards;
+	}
+	@keyframes bookmarkBounce {
+		0% {
+			transform: scale(1, 1);
+		}
+		25% {
+			transform: scale(0.9, 1.3);
+		}
+		50% {
+			transform: scale(1.1, 0.9);
+		}
+		75% {
+			transform: scale(0.95, 1.05);
+		}
+		100% {
+			transform: scale(1, 1);
+		}
 	}
 	@keyframes checkAnimation {
 		0% {
