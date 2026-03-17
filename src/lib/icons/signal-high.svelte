94db37e1d73a774f563e0db89137d6cad18d712a
jis3r <jis3r@protonmail.com>
2026-03-17 19:37:44 +0100

    fix: improved signal-high animation

--- src/lib/icons/signal-high.svelte
+++ src/lib/icons/signal-high.svelte
@@ -14,7 +14,7 @@
 		animate = true;
 		setTimeout(() => {
 			animate = false;
-		}, 700);
+		}, 1200);
 	}
 </script>
 
@@ -29,13 +29,15 @@
 		stroke-width={strokeWidth}
 		stroke-linecap="round"
 		stroke-linejoin="round"
-		class="signal-icon"
+		class="signal-high-icon"
 		class:animate
 	>
-		<path d="M2 20h.01" />
-		<path d="M7 20v-4" class="signal-level signal-line-1" />
-		<path d="M12 20v-8" class="signal-level signal-line-2" />
-		<path d="M17 20V8" class="signal-level signal-line-3" />
+		<g class="signal-high-group">
+			<path d="M2 20h.01" class="signal-high-path1" />
+			<path d="M7 20v-4" class="signal-high-path2" />
+			<path d="M12 20v-8" class="signal-high-path3" />
+			<path d="M17 20V8" class="signal-high-path4" />
+		</g>
 	</svg>
 </div>
 
@@ -43,40 +45,73 @@
 	div {
 		display: inline-block;
 	}
-	.signal-icon {
+	.signal-high-icon {
 		overflow: visible;
 	}
 
-	.signal-level {
+	.signal-high-path1,
+	.signal-high-path2,
+	.signal-high-path3,
+	.signal-high-path4 {
 		opacity: 1;
-		transition: opacity 0.2s ease;
+		stroke-dasharray: 1;
+		stroke-dashoffset: 0;
 	}
 
-	.signal-icon.animate .signal-level {
-		animation: fadeInSequence 0.6s ease forwards;
+	.signal-high-path1 {
+		stroke-dasharray: 0.5;
 	}
 
-	.signal-icon.animate .signal-line-1 {
+	.signal-high-path2 {
+		stroke-dasharray: 4;
+	}
+
+	.signal-high-path3 {
+		stroke-dasharray: 8;
+	}
+
+	.signal-high-path4 {
+		stroke-dasharray: 12;
+	}
+
+	.animate .signal-high-path1 {
 		opacity: 0;
-		animation-delay: 0.1s;
+		stroke-dashoffset: 0.5;
+		animation: signalHighPathAnimation 0.3s ease-in-out forwards;
+		animation-delay: 0s;
 	}
 
-	.signal-icon.animate .signal-line-2 {
+	.animate .signal-high-path2 {
 		opacity: 0;
+		stroke-dashoffset: 4;
+		animation: signalHighPathAnimation 0.3s ease-in-out forwards;
 		animation-delay: 0.2s;
 	}
 
-	.signal-icon.animate .signal-line-3 {
+	.animate .signal-high-path3 {
 		opacity: 0;
-		animation-delay: 0.3s;
+		stroke-dashoffset: 8;
+		animation: signalHighPathAnimation 0.3s ease-in-out forwards;
+		animation-delay: 0.4s;
 	}
 
-	@keyframes fadeInSequence {
+	.animate .signal-high-path4 {
+		opacity: 0;
+		stroke-dashoffset: 12;
+		animation: signalHighPathAnimation 0.3s ease-in-out forwards;
+		animation-delay: 0.6s;
+	}
+
+	@keyframes signalHighPathAnimation {
 		0% {
 			opacity: 0;
 		}
+		1% {
+			opacity: 1;
+		}
 		100% {
 			opacity: 1;
+			stroke-dashoffset: 0;
 		}
 	}
 </style>
